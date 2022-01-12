597399a7a119b643f644fc8013f2f8df88444e59
MaelDesigner <75177133+maeldesigner@users.noreply.github.com>
2022-01-12 13:13:40 +0100

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    music.playMelody("F A B G B - F D ", 120)
+})
